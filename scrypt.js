cb17b9f6efaf52050abd0d9840fa938a48715a45
vzvenygor <vzvenygorv@gmail.com>
2020-09-01 18:43:25 +0300

    Added js

---
+++ scrypt.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('.waxom-counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.waxom-counter-item-number').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+        {
+          duration: 1500,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+          }
+        });
+    });
+    a = 1;
+  }
+});
+});
